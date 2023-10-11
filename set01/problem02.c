9bf48c7c53deb43ee72c7df698554d7bfc24c222
Shreyasrinath21 <141308068+Shreyasrinath21@users.noreply.github.com>
2023-10-11 11:16:32 +0530

    committed 10/11/2023, 11:16:31 AM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,12 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the value of a and b:\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("the sum is %d\n", sum);
+    return  0;
+    
+}
