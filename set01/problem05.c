7c7af5da4e2057b2451533a584c483d8c0dd4865
Shreyasrinath21 <141308068+Shreyasrinath21@users.noreply.github.com>
2023-10-15 14:10:58 +0530

    committed 10/15/2023, 2:10:57 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,46 @@
-
+//Write a C program to compare three numbers using pass by value
+#include<stdio.h>
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+int main()
+{
+  int a,b,c,largest;
+  a=input();
+  b=input();
+  c=input();
+  compare(a,b,c);
+  output(a,b,c,largest);
+  return 0;
+}
+int input()
+{
+    int x;
+    printf("Enter the values:\n");
+    scanf("%d",&x);
+    return x;
+}
+int compare(int a, int b, int c)
+{
+    int largest;
+    if (a >= b && a >= c) 
+    {
+       largest=a;
+        return largest;
+    } 
+     if (b >= a && b >= c) 
+     {
+        largest=b;
+        return largest;
+     } 
+     else 
+     {
+        largest=c;
+        return largest;
+     }
+    return largest;
+}
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest number between %d %d and %d is %d\n",a,b,c,largest);
+}
