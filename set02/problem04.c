ba378041d1c63be9b389ed1ae2cb2a71103f2ae9
Shreyasrinath21 <141308068+Shreyasrinath21@users.noreply.github.com>
2023-10-18 22:06:17 +0530

    committed 10/18/2023, 10:06:16 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -57,7 +57,7 @@ int sum_composite_numbers(int n, int a[n]) {
     int i,sum = 0,result;
    for(i=0;i<n;i++) 
     {
-        if(result==(a[i])) {
+        if(result==1) {
             sum += a[i];
         }
     }
